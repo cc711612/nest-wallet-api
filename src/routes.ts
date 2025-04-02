88bb8dc9ab4c0d18b00222a0f4f0203adefdc559
Roy <cc711612@gmail.com>
2025-04-02 14:59:57 +0800

    exclude category jwt auth

--- src/routes.ts
+++ src/routes.ts
@@ -14,6 +14,7 @@ export function configureMiddlewares(consumer: MiddlewareConsumer) {
     .apply(JwtAuthMiddleware)
     .exclude(
       { path: 'wallets/:code/users', method: RequestMethod.GET },
+      { path: 'categories', method: RequestMethod.GET },
     )
     .forRoutes(
       { path: 'wallets', method: RequestMethod.ALL },
